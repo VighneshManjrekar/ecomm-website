a983df4cfbbafd1e136ee4da4ef099220c528710
devendra-dantal04 <devendra.dantal04@gmail.com>
2022-12-19 22:45:14 +0530

    Added Product Section

--- js/index.js
+++ js/index.js
@@ -1,15 +1,25 @@
 const openIcon = document.getElementById("open-icon");
 const closeIcon = document.getElementById("close-icon");
 const navItems = document.querySelector('.nav-list');
+const navItem = document.querySelectorAll('.nav-item');
+
 
 openIcon.addEventListener('click', () => {
         openIcon.style.display ="none";
         closeIcon.style.display ="block";
-        navItems.style.right = "0px"
+        // navItems.style.right = "0px"
+
+        for(i=0; i< navItem.length; i++) {
+                navItem[i].style.display = 'block';
+        }
+        navItems.style.width = "70vw";
 })
 
 closeIcon.addEventListener('click', () => {
         closeIcon.style.display ="none";
         openIcon.style.display ="block";
-        navItems.style.right = "-70vw"
+        navItems.style.width = "0px";
+        for(i=0; i< navItem.length; i++) {
+                navItem[i].style.display = 'none';
+        }
 })
